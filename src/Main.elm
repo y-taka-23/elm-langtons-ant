abf0c1e135ffe2b3491f7f324e7f50d588e62305
Yuto Takahashi <ytaka23dev@gmail.com>
2023-03-17 19:41:54 +0900

    Add step counter and button to increment

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,49 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
-main : Html msg
+
+-- Model
+
+type alias Model =
+    { steps : Int }
+
+
+-- Initial model
+
+initialModel : Model
+initialModel =
+    { steps = 0 }
+
+
+-- Msg
+
+type Msg
+    = Increment
+
+
+-- Update
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | steps = model.steps + 1 }
+
+
+-- View
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div [] [ text ("Steps: " ++ String.fromInt model.steps) ]
+        , button [ onClick Increment ] [ text "Click me!" ]
+        ]
+
+
+-- Main
+
 main =
-    text "Hello, Elm!"
+    Browser.sandbox { init = initialModel, update = update, view = view }
